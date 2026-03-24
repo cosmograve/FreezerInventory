0be11ec2e90d0862f72b3c5c0270d17bed20afe0
Aver Alexey <68560245+cosmograve@users.noreply.github.com>
2026-03-24 20:48:06 +0700

    Initial Commit

---
+++ FreezerInventory/FreezerInventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreezerInventoryApp.swift
+//  FreezerInventory
+//
+//  Created by Алексей Авер on 24.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct FreezerInventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
